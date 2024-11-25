6425941113ec25e3c178c038d6521405ed9ed894
Dastrilla <dastrilla@yandex.ru>
2024-11-25 15:28:57 +1100

    create prjoects

---
+++ Library.py
@@ -0,0 +1,109 @@
+import os
+import json
+
+
+#path to data file
+DATA_FILE = 'lib.json'
+
+
+class Book:
+
+    def __init__(self, title:str, author:str, year:int,
+                 id: int = None, status: str = "в наличии") -> None:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
+    
+    def to_dict(self) -> dict:
+        return self.__dict__
+    
+    @staticmethod
+    def from_dict(data: dict)-> 'Book':
+        return Book(**data)
+    
+class Library:
+
+    def __init__(self) -> None:
+        self.books = Library.load_books()
+
+    @staticmethod
+    def save_books(books: list['Book']):
+        with open(DATA_FILE, 'w', encoding='utf-8') as DB:
+            json.dump([Book.to_dict(book) for book in books], DB, ensure_ascii=False, indent=4)
+
+    @staticmethod
+    def load_books() -> list['Book']:
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, 'r', encoding='utf-8') as DB:
+                    return [Book.from_dict(book) for book in json.load(DB)]
+        return []
+
+    def generator_id(self) -> int:
+        if self.books:
+            return self.books[-1].id + 1
+        return 1
+    
+    def add_book(self, title:str, author:str, year:int) -> None:
+        book = Book(title, author, year, id = self.generator_id())
+        self.books.append(book)
+        Library.save_books(self.books)
+        print(f"Книга {book.title} добавлена с ID {book.id}.")
+
+    def delete_book(self, id: int) -> None:
+        for book in self.books:
+            if book.id == id:
+                self.books.remove(book)
+                print(f"Книга с ID:{id} удалена.")
+                Library.save_books(self.books)
+                return None
+        print(f"Книги с ID:{id} не существует.")
+
+    def search_book(self, search_field: str, query: str) -> None: 
+
+        match search_field:
+            case "1":
+                result =[book for book in self.books if query.lower() in book.title.lower()]
+            case "2":
+                result =[book for book in self.books if query.lower() in book.author.lower()]
+            case "3":
+                result =[book for book in self.books if int(query) == book.year]
+            case _:
+                print("Неправильное поле для поиска, убедитесь в корректном выборе.")
+
+        if result:
+            self.show_books(result)
+        else:
+            print("Ничего не найдено :(")
+            
+    def show_books(self, books: list["Book"] = None) -> None:
+        headers = self.books[0].__dict__.keys()
+        row_format = "{:<15} " * len(headers)
+        if books is None:
+            if len(self.books) == 0:
+                print("Библиотека пустая!")
+                return None
+            print(row_format.format(*headers))
+            for book in self.books:
+                print(row_format.format(*book.__dict__.values()))
+            return None
+        print(row_format.format(*headers))
+        for book in books:
+            print(row_format.format(*book.__dict__.values()))
+
+    def change_book_status(self, id: int, status: str) -> None:
+        for book in self.books:
+            if book.id == id:
+                if status != book.status:
+                    book.status = status.lower()
+                    Library.save_books(self.books)
+                    print(f"Изменен статус книги с ID({id}):{status}")
+                    return None
+                print("Такой статус уже установлен")
+                return None
+        print(f"Книги с ID:{id} не существует.")
+
+    @staticmethod
+    def exit()-> None:
+        quit()
---
+++ app.py
@@ -0,0 +1,68 @@
+import datetime as dt
+import Library
+
+def main()-> None:
+    """
+    """
+    print("Добро пожаловать в систему управление библиотекой!")
+    my_library = Library.Library()
+    while True:
+        print("\nМеню управление библиотекой:")
+        print("1. Добавить книгу")
+        print("2. Удалить книгу")
+        print("3. Искать книгу")
+        print("4. Показать все книги")
+        print("5. Изменить статус книги")
+        print("6. Выход")
+
+        user_input = input("\n").strip()[0]
+
+        match user_input:
+            case "1":
+                title = input("Введите название книги: ").strip()
+                author = input("Введите автора книги: ").strip()
+                year = input("Введите год издания книги: ").strip()
+                if not year.isdigit() or int(year) < 0 or int(year) > dt.datetime.now().year:
+                    print("Неккоректный год.")
+                    continue
+                my_library.add_book(title, author, int(year))
+
+            case "2":
+                book_id = input("Введите ID книги для удаления: ").strip()
+                if not book_id.isdigit() and int(book_id) < 0:
+                    print("Неккоректный ввод ID.")
+                    continue
+                my_library.delete_book(int(book_id))
+            
+            case "3":
+                search_field = input("Введите поле для поиска:\n"
+                                     "1. Заголовок\n"
+                                     "2. Автор\n"
+                                     "3. Год\n")
+                query = input("Введи данные: ")
+                print("Результат поиска: ")
+                my_library.search_book(search_field, query)
+
+            case "4":
+                my_library.show_books()
+
+            case "5":
+                book_id = input("Введите ID книги для изменения статуса: ").strip()
+                if not book_id.isdigit() and int(book_id) < 0:
+                    print("Неккоректный ввод ID.")
+                    continue
+                status = input("Введите новый статус книги (в наличии/выдана): ").strip()
+                if status not in ("в наличии", "выдана"):
+                    print("Некоректный ввод статуса")
+                    continue
+                my_library.change_book_status(int(book_id), status)
+
+            case "6":
+                print("Выход из системы управление библиотекой.")
+                my_library.exit()
+
+            case _:
+                print("Что-то пошло не так.\nУбедитесь в корректности запроса.")
+
+if __name__ == "__main__":
+    main()
